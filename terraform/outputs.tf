7450aa442013210c181c500bfc468328502b2694
reborn <lamponhq123@gmail.com>
2026-05-21 20:58:56 +0700

    Oh my god i forgot the output [deploy-aws]

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -7,8 +7,3 @@ output "cluster_endpoint" {
   description = "EKS cluster API endpoint"
   value       = aws_eks_cluster.eks.endpoint
 }
-
-output "eks_admin_role_arn" {
-  description = "IAM role ARN that can administer the EKS cluster"
-  value       = aws_iam_role.eks_admin.arn
-}
